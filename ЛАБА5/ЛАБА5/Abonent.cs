7359be17f6ed7afc1fdbdc385c925804b9bc36ec
ILYA222213 <117897751+ILYA222213@users.noreply.github.com>
2023-02-08 12:46:16 +0300

    Add files via upload

---
+++ ЛАБА5/ЛАБА5/Abonent.cs
@@ -0,0 +1,27 @@
+﻿
+
+public class Abonent
+{
+    string FullName = "Unknow";
+    double Address = 0;
+    double Number = 0;
+    int duration_city = 0;
+    int duration_intercity = 0;
+
+
+
+    public Abonent(string FullName, double Address, double Number, int duration_city, int duration_intercity)
+    {
+        this.FullName = FullName;
+        this.Address = Address;
+        this.Number = Number;
+        this.duration_city = duration_city;
+        this.duration_intercity = duration_intercity;
+    }
+
+    public void toString(string FullName, double Address, double Number, int duration_city, int duration_intercity)
+
+    {
+        Console.WriteLine("ФИО: " + FullName + "Адресс: " + Address + "Номер: " + Number + "Времени внутригородских разговоров: " + duration_city + "Пользование междугородней связи" + duration_intercity);
+    }
+}
