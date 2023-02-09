9bc1043093832f72b40748d7cf5e184009668ebb
ILYA222213 <117897751+ILYA222213@users.noreply.github.com>
2023-02-09 22:08:40 +0300

    Update Program.cs

--- ЛАБА5/ЛАБА5/Program.cs
+++ ЛАБА5/ЛАБА5/Program.cs
@@ -1,2 +1,14 @@
 ﻿
-    
+   using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        public void ScanAbonent(Abonent abonent)
+        {
+            Console.WriteLine("Введите ФИО: ");
+            
+            
+        }
+    }
